b22a4f3d79b58b27470f62358b80adfff7d1a224
Motaz Saad <motaz.saad@gmail.com>
2017-04-21 14:22:34 +0300

    process json files in input dir and write results to out dir

--- json2text.py
+++ json2text.py
@@ -1,27 +1,42 @@
 import json
-import sys
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description='extract tweet texts from json')
-parser.add_argument('-i', '--json-file', type=argparse.FileType(mode='r', encoding='utf-8'),
-help='tweets json file', required=True)
-parser.add_argument('-o', '--outfile', type=argparse.FileType(mode='w', encoding='utf-8'),
-help='the output file.', required=True)
+parser.add_argument('-i', '--json-dir', type=str,
+help='tweets json directory', required=True)
+parser.add_argument('-o', '--out-dir', type=str,
+help='the output directory.', required=True)
 
 
-def extract_tweets_from_json(json_reader, json_writer):
+def extract_tweets_from_json(json_reader, text_writer):
     json_tweets = json_reader.readlines()
     for json_tweet in json_tweets:
-        if json_tweets:
-            tweet = json.loads(json_tweet)  # load it as Python dict
-            text = tweet['text']
-            text = text.replace("\n", " ").strip()
-            json_writer.write(text)
-            json_writer.write("\n")
+        try:
+            if json_tweets:
+                tweet = json.loads(json_tweet)  # load it as Python dict
+                text = tweet['text']
+                text = text.replace("\n", " ").strip()
+                text_writer.write(text)
+                text_writer.write("\n")
+        except json.decoder.JSONDecodeError as error:
+            pass
+
+
+def extract_tweets_from_json_files(json_dir, text_dir):
+    for json_file in os.listdir(json_dir):
+        json_file = os.path.join(json_dir, json_file)
+        filename, ext = os.path.splitext(json_file)
+        text_file = os.path.join(text_dir, os.path.basename(filename) + '.txt')
+        with open(json_file, mode='r', encoding='utf-8') as json_reader, open(text_file, mode='w', encoding='utf-8') as text_writer:
+            print('extract tweets from {}'.format(json_file))
+            extract_tweets_from_json(json_reader, text_writer)
+            print('tweets extracted to {}'.format(text_file))
+            print('--------------------')
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    json_file = args.json_file
-    json_out = args.outfile
-    extract_tweets_from_json(json_file, json_out)
+    json_dir = args.json_dir
+    out_dir = args.out_dir
+    extract_tweets_from_json_files(json_dir, out_dir)
