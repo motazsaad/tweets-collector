0d24ed90159af261af562c323f0b07798d280000
Motaz Saad <motaz.saad@gmail.com>
2017-04-23 07:55:44 +0300

    iterate only on .json files

--- json2text.py
+++ json2text.py
@@ -2,6 +2,7 @@ import json
 import argparse
 import os
 import sys
+import glob
 
 parser = argparse.ArgumentParser(description='extract tweet texts from json')
 parser.add_argument('-i', '--json-dir', type=str,
@@ -28,16 +29,15 @@ def extract_tweets_from_json(json_reader, text_writer):
 
 def extract_tweets_from_json_files(json_dir, text_dir):
     for json_file in os.listdir(json_dir):
-        if json_file.startswith('.'):
-            continue
-        json_file = os.path.join(json_dir, json_file)
-        filename, ext = os.path.splitext(json_file)
-        text_file = os.path.join(text_dir, os.path.basename(filename) + '.txt')
-        with open(json_file, mode='r', encoding='utf-8') as json_reader, open(text_file, mode='w', encoding='utf-8') as text_writer:
-            print('extract tweets from {}'.format(json_file))
-            extract_tweets_from_json(json_reader, text_writer)
-            print('tweets extracted to {}'.format(text_file))
-            print('--------------------')
+        if json_file.endswith('.json'):
+            json_file = os.path.join(json_dir, json_file)
+            filename, ext = os.path.splitext(json_file)
+            text_file = os.path.join(text_dir, os.path.basename(filename) + '.txt')
+            with open(json_file, mode='r', encoding='utf-8') as json_reader, open(text_file, mode='w', encoding='utf-8') as text_writer:
+                print('extract tweets from {}'.format(json_file))
+                extract_tweets_from_json(json_reader, text_writer)
+                print('tweets extracted to {}'.format(text_file))
+                print('--------------------')
 
 
 if __name__ == '__main__':
@@ -45,6 +45,6 @@ if __name__ == '__main__':
     json_dir = args.json_dir
     out_dir = args.out_dir
     if json_dir == out_dir:
-        print('error: input and output directories can not be the same')
+        print('error: input and output directories can not be the same.')
         sys.exit(-1)
     extract_tweets_from_json_files(json_dir, out_dir)
