c404bfce08a580808399dc68a4b7798e2a610f45
Motaz Saad <motaz.saad@gmail.com>
2018-09-23 22:24:18 +0300

    fix tweet limit to 140 chars and organize code

--- user_tweets_history.py
+++ user_tweets_history.py
@@ -8,6 +8,7 @@ import csv
 import api_keys
 import xlsxwriter
 import tweet_cleaner
+import json
 
 
 def get_all_tweets(screen_name):
@@ -23,46 +24,41 @@ def get_all_tweets(screen_name):
     api = tweepy.API(auth)
 
     # initialize a list to hold all the tweepy Tweets
-    all_tweets = []
+    alltweets = []
 
     # make initial request for most recent tweets (200 is the maximum allowed count)
-    new_tweets = api.user_timeline(screen_name=screen_name, count=200, tweet_mode='extended')
+    new_tweets = api.user_timeline(screen_name=screen_name, count=200,
+                                   tweet_mode='extended')
 
     # save most recent tweets
-    all_tweets.extend(new_tweets)
+    alltweets.extend(new_tweets)
 
     # save the id of the oldest tweet less one
-    oldest = all_tweets[-1].id - 1
+    oldest = alltweets[-1].id - 1
 
     # keep grabbing tweets until there are no tweets left to grab
     while len(new_tweets) > 0:
         print("getting tweets before {}".format(oldest))
 
         # all subsiquent requests use the max_id param to prevent duplicates
-        new_tweets = api.user_timeline(screen_name=screen_name, count=200, max_id=oldest, tweet_mode='extended')
+        new_tweets = api.user_timeline(screen_name=screen_name, count=200,
+                                       max_id=oldest, tweet_mode='extended')
 
         # save most recent tweets
-        all_tweets.extend(new_tweets)
+        alltweets.extend(new_tweets)
 
         # update the id of the oldest tweet less one
-        oldest = all_tweets[-1].id - 1
+        oldest = alltweets[-1].id - 1
 
-    print("...{} tweets downloaded so far".format((len(all_tweets))))
-    #print('all tweets\n', all_tweets)
-    #print('first tweet:', all_tweets[0])
-    # transform the tweepy tweets into a 2D array that will populate the csv
-    outtweets = [[tweet.id_str, tweet.created_at.strftime('%m/%d/%Y'), tweet.text.encode("utf-8").decode('utf-8')] for tweet in all_tweets]
-    outtweetsDict = [{'id': tweet.id_str, 'created_at': tweet.created_at.strftime('%m/%d/%Y'), 'text':  tweet.text.encode("utf-8").decode('utf-8')} for
-                 tweet in all_tweets]
-    #print('first outtweets:', outtweets[0])
+    print("...{} tweets downloaded so far".format((len(alltweets))))
+    with open('{}.json'.format(screen_name), 'w') as outfile:
+        for t in alltweets:
+            outfile.write(json.dumps(t._json))
+            outfile.write('\n')
+    print('{} tweets have been written successfully to {}.json'.format((len(alltweets)), screen_name))
 
-    # write the csv
-    with open('%s_tweets.csv' % screen_name, 'w') as csvfile:
-        fieldnames = ["id", "created_at", "text"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, dialect='excel')
-        writer.writeheader()
-        writer.writerows(outtweetsDict)
 
+def process_json(screen_name):
     workbook = xlsxwriter.Workbook('%s_tweets.xlsx' % screen_name)
     worksheet = workbook.add_worksheet()
     # Start from the first cell. Rows and columns are zero indexed.
@@ -70,21 +66,31 @@ def get_all_tweets(screen_name):
     col = 0
     worksheet.write(row, 0, 'id')
     worksheet.write(row, 1, 'created_at')
-    worksheet.write(row, 2, 'original_text')
+    worksheet.write(row, 2, 'full_text')
     worksheet.write(row, 3, 'clean_text')
     row += 1
-    for tid, tdate, text in outtweets:
-        clean_text = tweet_cleaner.clean_tweet(text)
-        clean_text = tweet_cleaner.normalize_arabic(clean_text)
-        clean_text = tweet_cleaner.remove_repeating_char(clean_text)
-        clean_text = tweet_cleaner.keep_only_arabic(clean_text.split())
-        worksheet.write(row, col, tid)
-        worksheet.write(row, col + 1, tdate)
-        worksheet.write(row, col + 2, text)
-        worksheet.write(row, col + 3, clean_text)
-        row += 1
-    workbook.close()
+
+    with open('{}.json'.format(screen_name)) as json_reader:
+        lines = json_reader.readlines()
+        for line in lines:
+            json_tweet = json.loads(line)
+            if 'retweeted_status' in json_tweet:
+                text = json_tweet['retweeted_status']['full_text']
+            else:
+                text = json_tweet['full_text']
+            clean_text = tweet_cleaner.clean_tweet(text)
+            clean_text = tweet_cleaner.normalize_arabic(clean_text)
+            clean_text = tweet_cleaner.remove_repeating_char(clean_text)
+            clean_text = tweet_cleaner.keep_only_arabic(clean_text.split())
+            worksheet.write(row, col, json_tweet['id_str'])
+            worksheet.write(row, col + 1, json_tweet['created_at'])
+            worksheet.write(row, col + 2, text)
+            worksheet.write(row, col + 3, clean_text)
+            row += 1
+        workbook.close()
+
 
 if __name__ == '__main__':
     # pass in the username of the account you want to download
     get_all_tweets("Motaz_K_Saad")
+    process_json("Motaz_K_Saad")
